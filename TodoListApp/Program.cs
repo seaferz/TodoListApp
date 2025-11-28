f2f36fff6488718b663e2ea2f0755535f1cda56e
TaskOne <xxxchebmakxxx@gmail.com>
2025-11-28 15:41:53 +0300

    Refactor: Update welcome message differently on mastr

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using TodoListApp;
 using TodoListApp.Logic;
-Console.WriteLine("Welcom e to the To-Do List Application!");
+Console.WriteLine("Welcome t o the To-Do List Application!");
 
 var todoManager = new TodoListManager();
 
